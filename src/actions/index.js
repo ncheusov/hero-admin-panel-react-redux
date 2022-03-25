b6a32c170dba2f1b992beedba5dbbd040863ec51
Nikita Cheusov <ncheusov@inbox.ru>
2022-03-25 21:43:27 +0300

    complete filter using createSlice

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
-
-import { heroesFetching, heroesFetched, heroesFetchingError } from '../components/heroesList/heroesSlice'
+import { filtersFetching, filtersFetched, filtersFetchingError} from '../components/heroesFilters/filtersSlice';
+import { heroesFetching, heroesFetched, heroesFetchingError } from '../components/heroesList/heroesSlice';
 
 export const fetchHeroes = (request) => (dispatch) => {
     dispatch(heroesFetching());
@@ -14,38 +14,3 @@ export const fetchFilters = (request) => (dispatch) => {
         .then(data => dispatch(filtersFetched(data)))
         .catch(() => dispatch(filtersFetchingError()))
 }
-
-export const filtersFetching = () => {
-    return {
-        type: 'FILTERS_FETCHING'
-    }
-}
-
-export const filtersFetched = (filters) => {
-    return {
-        type: 'FILTERS_FETCHED',
-        payload: filters
-    }
-}
-
-export const filtersFetchingError = () => {
-    return {
-        type: 'FILTERS_FETCHING_ERROR'
-    }
-}
-
-export const activeFilterChanged = (filter) => {
-    return {
-        type: 'ACTIVE_FILTER_CHANGED',
-        payload: filter
-    }
-}
-
-// export const activeFilterChanged = (filter) => (dispatch) => {
-//     setTimeout(() => {
-//         dispatch({
-//             type: 'ACTIVE_FILTER_CHANGED',
-//             payload: filter
-//         })
-//     }, 1000)
-// }
